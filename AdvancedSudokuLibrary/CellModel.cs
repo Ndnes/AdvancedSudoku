1e67a02542251d23b763dade88915911eea7ac5d
Terje Molnes <tmolnez@gmail.com>
2019-05-18 05:25:14 +0200

    Added CellModel class

---
+++ AdvancedSudokuLibrary/CellModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AdvancedSudokuLibrary
+{
+    public class CellModel : INotifyPropertyChanged
+    {
+
+
+
+    }
+}
