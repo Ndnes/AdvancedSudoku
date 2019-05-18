353692f49c6131c723c267cbc74f52554fc74d35
Terje Molnes <tmolnez@gmail.com>
2019-05-18 05:29:25 +0200

    Added enum CellStateEnum

---
+++ AdvancedSudokuLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedSudokuLibrary
+{
+    public enum CellStateEnum
+    {
+        Blank,
+        Answer,
+        Hint,
+        UserInputCorrect,
+        UserInputWrong,
+        Notes
+    }
+}
