24de43f023af22a26440574142211932adf3d3c0
Terje Molnes <tmolnez@gmail.com>
2019-05-18 05:42:14 +0200

    Implemented interface INotifyPropertyChanged in CellModel

--- AdvancedSudokuLibrary/CellModel.cs
+++ AdvancedSudokuLibrary/CellModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace AdvancedSudokuLibrary
@@ -8,7 +9,35 @@ namespace AdvancedSudokuLibrary
     public class CellModel : INotifyPropertyChanged
     {
 
+        private CellStateEnum _cellState;
+
+        public CellStateEnum CellState
+        {
+            get { return _cellState; }
+            set
+            {
+                _cellState = value;
+                OnPropertyChanged();
+            }
+        }
 
 
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged; //Interface definition
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") //TODO: What is CallerMemberName?
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
     }
 }
